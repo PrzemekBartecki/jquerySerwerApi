2190370ebe419fd1ecce18ae48b01f332fd5925a
przemyslaw.bartecki <przemyslaw.bartecki@apricot.com.pl>
2021-07-20 10:39:04 +0200

    start code

---
+++ ui/src/index.js
@@ -0,0 +1,108 @@
+import 'bootstrap'
+import './scss/index.scss'
+
+
+$.ajax({
+        url: "http://localhost:4454/servers",
+        method: "get",
+        dataType: "json"
+    })
+    .done(res => {
+        const $serwersArray = res;
+
+        let $serversNubmer = $serwersArray.length
+        const $placeNumberSerwers = $("#numberSerwers span")
+        $placeNumberSerwers.append($serversNubmer)
+
+        showSerwers($serwersArray)
+    });
+
+function showSerwers(array) {
+    array.forEach(el => {
+        const $listServers = $(".servers-list")
+        const $listServersItem = $(
+            `<div class='servers-list-item'>
+                    <div class='name'>
+                        <p>${el.name}</p>
+                    </div>
+                    <div class='status'>
+                        <p>${el.status}</p>
+                    </div>
+                    <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     <span></span>
+                     <span></span>
+                     <span></span>
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <ul>
+
+                  </ul>
+                    </div>
+                  </div>
+                </div>`
+        );
+        $listServers.append($listServersItem);
+    });
+
+    addClass();
+    checkBtn();
+    switchStatus();
+
+}
+
+function addClass() {
+
+    $(".servers-list-item .status p").filter(function () {
+        return $(this).text() === "ONLINE"
+    }).addClass("online");
+
+    $(".servers-list-item .status p").filter(function () {
+        return $(this).text() === "OFFLINE"
+    }).addClass("offline")
+
+    $(".servers-list-item .status p").filter(function () {
+        return $(this).text() === "REBOOTING"
+    }).addClass("rebbooting")
+}
+
+function checkBtn() {
+    let btn = $(".dropdown button");
+
+    btn.click(function () {
+
+        let $test = $(this).parents(".servers-list-item")
+
+        if ($test.find(".status p").text() === "ONLINE") {
+            let $list = $(".dropdown-menu ul")
+            $list.find("li").remove();
+
+            $list.append(
+                `<li class="turn-off">Turn off</li>
+                 <li class="reboot">Reboot</li>`
+            );
+            switchStatus();
+
+        } else if ($test.find(".status p").text() === "OFFLINE") {
+            let $list = $(".dropdown-menu ul")
+            $list.find("li").remove();
+            $list.append(`<li class="turn-on">Turn on</li>`);
+        }
+    })
+
+
+}
+
+function switchStatus() {
+    let $btn = $(".dropdown button ul .turn-off");
+
+    $btn.click(function() {
+        console.log('działa',  $(this))
+    })
+
+}
+
+
+
+console.log($().jquery)
+console.log('dzoałam')
